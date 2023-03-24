12005e26f484735e530ecc7f623d2943e006d0cd
Anna Adamik <quenthel5@gmail.com>
2023-03-24 14:45:09 +0100

    Initial commit

---
+++ src/NextMain.java
@@ -0,0 +1,31 @@
+public class NextMain {
+
+    public static void main(String[] args) {
+
+        Movie theMovie = Movie.getMovie("A", "Jaws");
+        theMovie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+
+        Object unknownObject = Movie.getMovie("S", "Star Wars");
+        if (unknownObject.getClass().getSimpleName() == "Comedy") {
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if (unknownObject instanceof Adventure) {
+            ((Adventure) unknownObject).watchAdventure();
+        } else if (unknownObject instanceof ScienceFiction syfy) {
+            syfy.watchScienceFiction();
+        }
+    }
+}
